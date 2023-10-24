0ba3c1c84f6ea7033d84867ef24206931a8d1dbc
Sydney Cendana <103895652+sydneycendana@users.noreply.github.com>
2023-10-24 13:23:32 -0700

    error middleware

---
+++ API/Errors/APIException.cs
@@ -0,0 +1,20 @@
+﻿using System.Security.Cryptography;
+
+namespace API.Errors;
+
+public class APIException
+{
+    public APIException(int statusCode, string message, string details)
+    {
+        StatusCode = statusCode;
+        Message = message;
+        Details = details;
+    }
+
+    public int StatusCode { get; set; }
+    public string Message { get; set; }
+    public string Details { get; set; }
+
+
+
+}
