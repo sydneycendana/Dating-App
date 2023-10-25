b7685944a022f36e6a6c14d13a8374007111fc5b
Sydney Cendana <103895652+sydneycendana@users.noreply.github.com>
2023-10-25 11:25:59 -0700

    mapping

--- API/DTOs/MemberDTO.cs
+++ API/DTOs/MemberDTO.cs
@@ -1,14 +1,11 @@
-﻿using API.Extensions;
+﻿namespace API.DTOs;
 
-namespace API.Entities;
-
-public class AppUser
+public class MemberDTO
 {
     public int Id { get; set; }
     public string UserName { get; set; }
-    public byte[] PasswordHash { get; set; }
-    public byte[] PasswordSalt { get; set; }
-    public DateOnly DateOfBirth { get; set; }
+    public string PhotoUrl { get; set; }
+    public int Age { get; set; }
     public string KnownAs { get; set; }
     public DateTime Created { get; set; } = DateTime.UtcNow;
     public DateTime LastActive { get; set; }
@@ -18,11 +15,5 @@ public class AppUser
     public string Interests { get; set; }
     public string City { get; set; }
     public string Country { get; set; }
-    public List<Photo> Photos { get; set; } = new();
-
-    // public int GetAge()
-    // {
-    //     return DateOfBirth.CalculateAge();
-    // }
-
+    public List<PhotoDTO> Photos { get; set; }
 }
---
+++ API/DTOs/PhotoDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace API;
+
+public class PhotoDTO
+{
+    public int Id { get; set; }
+    public string Url { get; set; }
+    public bool IsMain { get; set; }
+}
